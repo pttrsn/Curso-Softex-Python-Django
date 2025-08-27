643d7ca6f17d397b0d14f2e8e7f2cafc8ac5fc0e
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-08-27 11:21:15 -0300

    Add encoding/decoding script and reorganize files
    Added a new script for encoding and decoding vowels in words. Moved telefone-related scripts from 'aula 08' to 'exercicios-python/telefone' for better organization.

---
+++ exercicios-python/codificacao-e-decodificacao/codficacao-descodificacao.py
@@ -0,0 +1,15 @@
+pal = input('Escreva uma palavra: ')
+
+cod = pal
+cod = cod.replace('a','1')
+cod = cod.replace('e','2')
+cod = cod.replace('i','3')
+cod = cod.replace('o','4')
+cod = cod.replace('u','5')
+
+dec = cod
+dec = dec.replace('1','a')
+dec = dec.replace('2','e')
+dec = dec.replace('3','i')
+dec = dec.replace('4','o')
+dec = dec.replace('5','u')
