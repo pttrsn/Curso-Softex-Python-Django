1ef006367c58e841b4945617b9329ebe1207b1b4
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-08-30 22:25:01 -0300

    Add triangle validation exercise and solution
    Added initial files for the triangle validation exercise, including an empty command file, an empty solution file, and implemented the logic in validacao-triangulo.py to check if three values can form a triangle and classify its type.

---
+++ exercicios-python/validacao-triangulo/validacao-triangulo.py
@@ -0,0 +1,22 @@
+while True:
+    v1 = float(input('Digite o 1° valor: '))
+    v2 = float(input('Digite o 2° valor: '))
+    v3 = float(input('Digite o 3° valor: '))
+
+    if v1 > 0 and v2 > 0 and v3 > 0:
+        if v1 < v2 + v3 and v2 < v1 + v3 and v3 < v1 + v2:
+            if v1 > (v2 - v3) and v2 > (v1 - v3) and v3 > (v1 - v2):
+                print('Os valores podem formar um triângulo.')
+                if v1 == v2 == v3:
+                    print('O triângulo formado é Equilátero.')
+                elif v1 == v2 or v2 == v3 or v3 == v1:
+                    print('O triângulo formado é Isósceles.')
+                else:
+                    print('O triângulo formado é Escaleno.')
+                break
+            else:
+                print('Os valores não obedecem a condição de diferença do triângulo.')
+        else:
+            print('Os valores não obedecem a condição de soma do triângulo.')
+    else:    
+        print('Valores inválidos.')
