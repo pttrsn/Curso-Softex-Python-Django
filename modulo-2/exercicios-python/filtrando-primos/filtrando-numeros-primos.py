631bd63a4fbc76c9c688a415059d37cd7d8f1633
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-09-01 11:19:22 -0300

    Update filtrando-numeros-primos.py

--- modulo-2/exercicios-python/filtrando-primos/filtrando-numeros-primos.py
+++ modulo-2/exercicios-python/filtrando-primos/filtrando-numeros-primos.py
@@ -2,10 +2,11 @@ lista1 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 primos = []
 
 
-primo = 
 
-for numero in range (1,9):
+
+for numero in lista1:
+    primo = numero % 1 == 0 and numero % numero == 0
     if numero == primo:
         primos.append(primo)
 
-print(primos)
+print(primos) 
