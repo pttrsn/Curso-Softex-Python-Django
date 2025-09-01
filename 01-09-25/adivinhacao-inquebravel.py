31f728ebab3a47f2b38e63bb3c1ed15fdf342171
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-09-01 11:16:24 -0300

    Remove unused code and add prime filter script
    Removed an incomplete conditional from adivinhacao-inquebravel.py. Added a new script filtrando-numeros-primos.py to filter prime numbers from a list, though the implementation is currently incomplete.

--- 01-09-25/adivinhacao-inquebravel.py
+++ 01-09-25/adivinhacao-inquebravel.py
@@ -1,5 +1,4 @@
 while True:
     numero = input('Escreva um número para eu adivinhar: ')      
     if numero.isdigit():
-        if numero.len()
         print(f'Seu número é {numero}?')
---
+++ modulo-2/exercicios-python/filtrando-primos/filtrando-numeros-primos.py
@@ -0,0 +1,11 @@
+lista1 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+primos = []
+
+
+primo = 
+
+for numero in range (1,9):
+    if numero == primo:
+        primos.append(primo)
+
+print(primos)
