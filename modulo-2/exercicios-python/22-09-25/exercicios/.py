61e64ce8c6d4ec37abe00e68805e5d29bc248af4
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-09-22 10:33:45 -0300

    Add type validation to Pessoa class and example file
    Improved the Pessoa class by updating the nome setter to validate that the name is a non-empty string. Added usage examples to test setter validation. Also added a new file demonstrating the use of the type() function in Python.

--- modulo-2/exercicios-python/22-09-25/exercicios/.py
+++ modulo-2/exercicios-python/22-09-25/exercicios/.py
@@ -3,7 +3,7 @@ Objetivo: Criar uma classe com validações simples nos setters para tipo e valo
 ●  Requisitos: 
 x 1.  Crie uma classe Pessoa com os atributos protegidos _nome e _idade. 
 x 2.  Crie uma @property para nome. 
-3.  Crie um @nome.setter que valide se o valor é uma string e não está vazio. 
+x 3.  Crie um @nome.setter que valide se o valor é uma string e não está vazio. 
 4.  Crie uma @property para idade. 
 5.  Crie um @idade.setter que valide se a idade é um número inteiro e positivo. 
 6.  No __init__, use os setters para atribuir os valores iniciais (ex: self.nome = nome). 
@@ -12,7 +12,7 @@ valor vazio e idade para um valor negativo para testar as validações.
 '''
 
 class Pessoa:
-    def __init__(self, nome, idade):
+    def __init__(self, nome:str, idade:int):
         self._nome = nome
         self._idade = idade
 
@@ -23,8 +23,20 @@ class Pessoa:
         return self._idade
     
     @nome.setter
-    def nome(self, nome_enc):
-        if nome_enc == str and nome_enc != '':
+    def nome(self, nome_enc:str):
+        if isinstance(nome_enc, str) and not nome_enc:
             self._nome = nome_enc
         else:
-            print('Nome inválido.')
+            print('Erro! O nome deve ser uma string não vazia.')
+
+pessoa_enc = Pessoa('João', '19')
+print(pessoa_enc.nome)
+
+pessoa_enc.nome = 'Rogério'
+print(pessoa_enc.nome)
+
+pessoa_enc.nome = ''
+print(pessoa_enc.nome)
+
+pessoa_enc.nome = 0
+print(pessoa_enc.nome)
---
+++ modulo-2/exercicios-python/22-09-25/exercicios/explicacao type.py
@@ -0,0 +1,14 @@
+valor = 'palavra'
+valor2 = 1
+valor3 = 1.0
+valor4 = True
+
+tipo = type(valor)
+tipo2 = type(valor2)
+tipo3 = type(valor3)
+tipo4 = type(valor4)
+
+print(tipo)
+print(tipo2)
+print(tipo3)
+print(tipo4)
