a2ba7714b941d0669b5d24470d7db25bd9f4c855
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-09-22 10:22:22 -0300

    Add property and setter for nome in Pessoa class
    Implemented the @property and @nome.setter for the Pessoa class, including validation to ensure the nome is a non-empty string. This addresses part of the exercise requirements for attribute encapsulation and validation.

--- modulo-2/exercicios-python/22-09-25/exercicios/.py
+++ modulo-2/exercicios-python/22-09-25/exercicios/.py
@@ -2,7 +2,7 @@
 Objetivo: Criar uma classe com validações simples nos setters para tipo e valor. 
 ●  Requisitos: 
 x 1.  Crie uma classe Pessoa com os atributos protegidos _nome e _idade. 
-2.  Crie uma @property para nome. 
+x 2.  Crie uma @property para nome. 
 3.  Crie um @nome.setter que valide se o valor é uma string e não está vazio. 
 4.  Crie uma @property para idade. 
 5.  Crie um @idade.setter que valide se a idade é um número inteiro e positivo. 
@@ -15,3 +15,16 @@ class Pessoa:
     def __init__(self, nome, idade):
         self._nome = nome
         self._idade = idade
+
+    @property
+    def nome(self):
+        return self._nome
+    def idade(self):
+        return self._idade
+    
+    @nome.setter
+    def nome(self, nome_enc):
+        if nome_enc == str and nome_enc != '':
+            self._nome = nome_enc
+        else:
+            print('Nome inválido.')
