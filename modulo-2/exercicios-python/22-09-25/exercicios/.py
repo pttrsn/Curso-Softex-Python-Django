020333adf10fcc0fba5072c05d181daf213f62f6
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-09-22 11:01:30 -0300

    Implement Pessoa class with property validations
    Added property and setter methods for 'nome' and 'idade' with validation logic. Updated instantiation and test cases to demonstrate validation for empty names and negative ages.

--- modulo-2/exercicios-python/22-09-25/exercicios/.py
+++ modulo-2/exercicios-python/22-09-25/exercicios/.py
@@ -4,10 +4,10 @@ Objetivo: Criar uma classe com validações simples nos setters para tipo e valo
 x 1.  Crie uma classe Pessoa com os atributos protegidos _nome e _idade. 
 x 2.  Crie uma @property para nome. 
 x 3.  Crie um @nome.setter que valide se o valor é uma string e não está vazio. 
-4.  Crie uma @property para idade. 
-5.  Crie um @idade.setter que valide se a idade é um número inteiro e positivo. 
-6.  No __init__, use os setters para atribuir os valores iniciais (ex: self.nome = nome). 
-7.  Instancie um objeto Pessoa com dados válidos e depois tente alterar nome para um 
+x 4.  Crie uma @property para idade. 
+x 5.  Crie um @idade.setter que valide se a idade é um número inteiro e positivo. 
+x 6.  No __init__, use os setters para atribuir os valores iniciais (ex: self.nome = nome). 
+x 7.  Instancie um objeto Pessoa com dados válidos e depois tente alterar nome para um 
 valor vazio e idade para um valor negativo para testar as validações.
 '''
 
@@ -19,24 +19,36 @@ class Pessoa:
     @property
     def nome(self):
         return self._nome
-    def idade(self):
-        return self._idade
     
     @nome.setter
     def nome(self, nome_enc:str):
-        if isinstance(nome_enc, str) and not nome_enc:
+        if isinstance(nome_enc, str) and not nome_enc == '': 
             self._nome = nome_enc
         else:
             print('Erro! O nome deve ser uma string não vazia.')
+    
+    @property
+    def idade(self):
+        return self._idade
 
-pessoa_enc = Pessoa('João', '19')
+    @idade.setter
+    def idade(self, idade_enc:int):
+        if isinstance(idade_enc, int) and not idade_enc < 0:
+            self._idade = idade_enc
+        else:
+            print('Erro! A idade deve ser um valor inteiro e não negativo.')
+
+
+
+
+pessoa_enc = Pessoa('João', 27)
 print(pessoa_enc.nome)
+print(pessoa_enc.idade)
 
 pessoa_enc.nome = 'Rogério'
+pessoa_enc.idade = 34
 print(pessoa_enc.nome)
+print(pessoa_enc.idade)
 
 pessoa_enc.nome = ''
-print(pessoa_enc.nome)
-
-pessoa_enc.nome = 0
-print(pessoa_enc.nome)
+pessoa_enc.idade = -10
