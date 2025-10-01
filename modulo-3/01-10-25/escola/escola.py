ef1d180744c41636bc651e3184549d7c9d271325
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-10-01 11:16:38 -0300

    escola

--- modulo-3/01-10-25/escola/escola.py
+++ modulo-3/01-10-25/escola/escola.py
@@ -1,12 +1,17 @@
 from estudante import Estudante
 
 class Escola:
-    estudantes = []
     def __init__(self):
-        self.estudante = Estudante()
+        self.estudantes = []
 
-    def adicionar_estudante(self):
-        pass
+    def adicionar_estudante(self, estudante):
+        if estudante not in self.estudantes:
+            self.estudantes[estudante] = []
+        self.estudantes[estudante].append(estudante)
 
     def mostrar_relatorio(self):
         pass
+
+escola = Escola()
+estudante1 = Estudante("Carlos", 16, "2025001")
+escola.adicionar_estudante(estudante1)
