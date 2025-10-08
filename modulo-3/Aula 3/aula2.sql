d6a189e8e346eaef7ef5a652d6b3e6c850ed1f7c
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-10-08 10:19:25 -0300

    Add user-course relationship and sample data to SQL
    Introduced the 'usuarios_cursos' table to represent the many-to-many relationship between users and courses. Updated the 'curso' table to use 'titulo' instead of 'nome'. Added sample data inserts and example queries for users, courses, and their relationships. Updated the database file accordingly.

--- modulo-3/Aula 3/aula2.sql
+++ modulo-3/Aula 3/aula2.sql
@@ -1,9 +1,30 @@
 CREATE TABLE usuario (
     id INTEGER PRIMARY KEY,
-    nome TEXT NOT NULL,
+    nome TEXT NOT NULL
 );
 
 CREATE TABLE curso (
     id INTEGER PRIMARY KEY,
-    nome TEXT NOT NULL,
+    titulo TEXT NOT NULL
 );
+
+CREATE TABLE usuarios_cursos (
+    id_usuario INTEGER NOT NULL,
+    id_curso INTEGER NOT NULL,
+    FOREIGN KEY(id_usuario) REFERENCES usuario(id),
+    FOREIGN KEY(id_curso) REFERENCES curso(id)
+);
+
+INSERT INTO usuario(nome) VALUES('Carlos'), ('Francisco'), ('Maria');
+INSERT INTO curso(titulo) VALUES ('Back-end'), ('Front-end');
+INSERT INTO usuario_curso(id_usuario, id_curso) VALUES (1,1), (1,2), (2,1);
+
+SELECT * FROM usuario;
+SELECT * FROM curso;
+SELECT * FROM usuario_curso;
+
+SELECT usuario.nome, curso.titulo FROM usuario
+INNER JOIN usuarios_cursos ON usuarios_cursos.id_usuario = usuario.id
+INNER JOIN curso AS Curso ON usuarios_cursos.id_curso = curso.id;
+
+SELECT count(*) FROM usuario;
