89506a6773a339876c8c01f36a80b9dafa3c4489
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-11-10 11:33:52 -0300

    Add Execucao model and display tasks and executions
    Introduces the Execucao model alongside Tarefa, updates migrations, and registers both models in the admin. The home view and template now display lists of tasks and executions from the database, improving data visibility on the main page.

--- modulo-4/core/admin.py
+++ modulo-4/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Tarefa, Execucao
 
-# Register your models here.
+admin.site.register(Tarefa)
+admin.site.register(Execucao)
--- modulo-4/core/views.py
+++ modulo-4/core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Tarefa
+from .models import Execucao
 
 def home(request):
+    todas_as_tarefas = Tarefa.objects.all()
+    todas_as_execucoes = Execucao.objects.all()
+    
     context = {
         'nome_usuario': 'Júnior',
-        'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
+        'tarefas': todas_as_tarefas,
+        'execucao': todas_as_execucoes,
     }
     return render(request, 'home.html', context)
-
-def login(request):
-    return HttpResponse('<input>Login</input>')
