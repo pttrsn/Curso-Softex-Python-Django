c03836c310aec55d4acaa8e1a7510def285b74ab
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-11-19 10:36:58 -0300

    Add login view and Tarefa model migration
    Introduces the initial migration for the Tarefa model, adds a login view using Django's built-in authentication, and creates a login template. Updates URL configuration to include the login route.

--- modulo-4/meuprojeto/urls.py
+++ modulo-4/meuprojeto/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import view as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
