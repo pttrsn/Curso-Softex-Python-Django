7d68f7f4c55c9c641d49b32f2d322a21b555bff3
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-11-24 10:40:58 -0300

    Implement user authentication and task ownership
    Added user authentication to the task management app, ensuring tasks are associated with the logged-in user. Updated views to filter and modify tasks per user, improved admin interface for Tarefa, and enhanced navigation and login/logout flows in templates. Renamed 'registro.html' to 'register.html' and cleaned up project and WSGI/ASGI files.

--- modulo-4/core/admin.py
+++ modulo-4/core/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Tarefa
 
-admin.site.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'user', 'concluida', 'criada_em')
+    list_filter = ('concluida', 'user', 'criada_em')
+    search_fields = ('titulo', 'user__username')
+
+admin.site.register(Tarefa,TarefaAdmin)
