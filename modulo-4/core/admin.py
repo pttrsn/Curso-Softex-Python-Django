09f1efa4260dbe2478d5cc8f0752dbf8f84e6b1c
pttrsn <164556343+pttrsn@users.noreply.github.com>
2025-11-24 11:05:10 -0300

    Add fieldsets and readonly field to TarefaAdmin
    Introduces fieldsets for better organization in the TarefaAdmin interface and sets 'criada_em' as a readonly field to prevent manual edits. This improves the admin experience and data integrity.

--- modulo-4/core/admin.py
+++ modulo-4/core/admin.py
@@ -5,5 +5,14 @@ class TarefaAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'user', 'concluida', 'criada_em')
     list_filter = ('concluida', 'user', 'criada_em')
     search_fields = ('titulo', 'user__username')
+    fieldsets = (
+('Informações Principais', {
+'fields': ('user', 'titulo')
+}),
+('Status da Tarefa', {
+'fields': ('concluida', 'criada_em')
+}),
+)
 
+readonly_fields = ('criada_em')
 admin.site.register(Tarefa,TarefaAdmin)
